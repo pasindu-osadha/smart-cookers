dc87ac69f607d7697fbe2cac086e57c8da9de4de
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-02 12:11:36 +0530

    install tailwind form plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -5,5 +6,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
