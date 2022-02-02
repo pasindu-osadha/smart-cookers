5b83b15e338ba82adb8e4f4766ddc9f7fd714492
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-02 09:05:38 +0530

    Add login component

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { LoginForm } from '../../components/login/LoginForm';
 
 export const Login = () => {
     return (
-        <div className='h-screen'>
-            <h1>Login page</h1>
+        <div className='h-screen ' >
+           <LoginForm />
         </div>
     );
 };
