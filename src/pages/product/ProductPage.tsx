2cf7ac164a8500295fd04fe875549c12826cb037
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-02 22:37:59 +0530

    add product page navigation buttons

--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ProductRouteBtn } from '../../components/product/ProductRouteBtn';
+import { AddProduct } from '../../containers/product/AddProduct';
 
 export const ProductPage = () => {
     return (
-        <div className='h-screen'>
-            <h1>Product page</h1>
+        <div className='h-screen bg-white flex justify-center'>
+
+            <div className='m-3'>
+                <Link to="/add-product" >
+                    <ProductRouteBtn name=' Add product' />
+                </Link>
+            </div>
+
+            <div className='m-3'>
+                <Link to="/view-all-product" >
+                    <ProductRouteBtn name=' View all Products' />
+                </Link>
+            </div>
+
+
+
         </div>
     );
 };
