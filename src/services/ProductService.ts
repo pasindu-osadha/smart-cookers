1de8171bc64ae14b3f642e07f993dc3a73585f25
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-02 11:30:49 +0530

    change mock api path

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import React from 'react';
 
-const api = 'https://61f7e4bd39431d0017eafae7.mockapi.io/';
+const api = 'https://61f91a50783c1d0017c44947.mockapi.io/';
 
 export const getAllProducts = () => {
-    console.log(api + 'products');
-    return axios.get(api + 'products');
+    console.log(api + 'product');
+    return axios.get(api + 'product');
 };
