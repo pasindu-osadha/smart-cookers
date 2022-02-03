90afa30a687476724d58c5ee69ecf8e3c53e8c94
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-03 11:32:30 +0530

    Add custermized color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'navbarColor': '#388087',
+      'lightNavbar': '#6FB3BB',
+      'lightBlue': '#BADFE7',
+      'lightGreen': '#C2EDCE',
+      'backgroundColor': '#F6F6F2'
+    },
     extend: {},
   },
   plugins: [
