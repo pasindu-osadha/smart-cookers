14b83d514e6d4388ffdf2aa3369474341afb9fee
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-08 11:11:38 +0530

    change footer text

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
     return (
         <div className="bottom-0 text-backgroundColor  text-center p-4 bg-footerColor">
-            <p>footer</p>
+            <p>Allion technologies</p>
         </div>
     );
 };
